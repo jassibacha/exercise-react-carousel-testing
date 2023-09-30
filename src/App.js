b5bbdcdc4026be035b37b7c4f90cc0f3964506e4
Jassi Bacha <klikster@gmail.com>
2023-09-29 17:49:17 -0700

    Init from fresh CRA and non buggy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './Carousel';
+import photos from './photos.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // In real life, this data would probably come from an AJAX call.
+    // For our purposes, we're just importing from another file.
+    const carouselPhotos = photos;
+    const carouselTitle = 'Shells from far-away beaches';
+
+    return (
+        <div className="App">
+            <Carousel photos={carouselPhotos} title={carouselTitle} />
+        </div>
+    );
 }
 
 export default App;
